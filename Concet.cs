604d0f66b6503199feaab29936735f1b01d6410e
konczdani0325 <konczd@kkszki.hu>
2024-09-26 10:17:51 +0200

    beolvas_1

---
+++ Concet.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoSale
+{
+    public class Concet
+    {
+        public MySqlConnection Connection;
+        private string Host;
+        private string Database;
+        private string User;
+        private string Password;
+        private string ConnectionString;
+
+        public Concet() 
+        { 
+            Host = "127.0.0.1";
+            Database = "auto";
+            User = "root";
+            Password = "0";
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
